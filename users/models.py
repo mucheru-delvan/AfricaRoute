08936fb7e0a05e47995ac61d26b72f5428e0486f
mucheru-delvan <mkdelvan9@gmail.com>
2026-09-15 06:36:11 +0300

    Applied migrations after creating a user class

--- users/models.py
+++ users/models.py
@@ -2,3 +2,20 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        DISPATCHER = "DISPATCHER", "Dispatcher"
+        DRIVER = "DRIVER", "Driver"
+        STAFF = "STAFF", "Staff"
+        
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.STAFF,
+    )
+    
+    phone_number = models.CharField(max_length=20, blank=True)
+    
+    def __str__(self):
+        return self.username
