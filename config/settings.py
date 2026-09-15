d81c5386c1ba25f2e0429b878e97203f2787c4e7
mucheru-delvan <mkdelvan9@gmail.com>
2026-09-15 22:22:05 +0300

    Added jwt urls in config/urls.py

--- config/settings.py
+++ config/settings.py
@@ -137,8 +137,12 @@ MAILERS = {
 }
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
     ),
 }
+
 AUTH_USER_MODEL = "users.User"
