ef64e86ca0a6728a8337df824e4e23956fe21bf5
mucheru-delvan <mkdelvan9@gmail.com>
2026-09-15 06:48:22 +0300

    I added comments in admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+#I added ignore comments to the code below to avoid type checking errors in my IDE.
+# The code is correct and works as expected.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("AfricaRoute", {"fields": ("role", "phone_number")}),
+    ) # type: ignore
 
-# Register your models here.
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("AfricaRoute", {"fields": ("role", "phone_number")}),
+    ) # type: ignore
+
+    list_display = UserAdmin.list_display + ("role", "phone_number") # type: ignore
+    list_filter = UserAdmin.list_filter + ("role",) # type: ignore
