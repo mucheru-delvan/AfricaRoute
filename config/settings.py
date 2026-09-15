94640878e27141be6b1a1b9fbe79a5442c6c356a
mucheru-delvan <mkdelvan9@gmail.com>
2026-09-15 06:00:04 +0300

    Created a users app

--- config/settings.py
+++ config/settings.py
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'drf-spectacular',
+    'drf_spectacular',
     'django_filters',
+    'users',
     
 ]
 
