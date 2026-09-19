3c5b79ee7c674e17c97fd9218942acc6b9b88eb6
mucheru-delvan <mkdelvan9@gmail.com>
2026-09-19 18:04:08 +0300

    Added JWT url for login

--- config/urls.py
+++ config/urls.py
@@ -25,6 +25,17 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("users.urls")),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
+    path(
+        "api/auth/login/",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),
+    
+    path(
+        "api/auth/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh",
+    ),
 ]
+
