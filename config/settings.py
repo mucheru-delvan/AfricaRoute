985daa0f4794c478e3ceded1ca60b3964d4df2ce
mucheru-delvan <mkdelvan9@gmail.com>
2026-09-19 19:22:48 +0300

    Increased the expiry duration of the tokens for development

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,6 +144,11 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
     ),
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=3000),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=5),
 }
 
 AUTH_USER_MODEL = "users.User"
--- users/views.py
+++ users/views.py
@@ -2,14 +2,14 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-
+from rest_framework.permissions import IsAuthenticated
 from users.permissions import IsDispatcher
 from .serializers import RegisterSerializer
 
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
-
+    #Create a new user
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
 
@@ -30,10 +30,22 @@ class RegisterView(APIView):
             status=status.HTTP_201_CREATED,
         )
 
-
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+    #Retrieve the authenticated user's information
+    def get(self, request):
+        return Response({
+            "id": request.user.id,
+            "username": request.user.username,
+            "email": request.user.email,
+            "role": request.user.role,
+            "phone_number": request.user.phone_number,
+        })
+        
+        
 class DispatchView(APIView):
     permission_classes = [IsDispatcher]
-
+    #Tests if the authenticated user has the dispatcher role
     def get(self, request):
         return Response({
             "message": "You are a dispatcher."
