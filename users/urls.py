90a809611b52dccb1c7b91e1856612241c8e68b1
mucheru-delvan <mkdelvan9@gmail.com>
2026-09-19 19:20:43 +0300

    Created dispatch url pattern

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import RegisterView
+from .views import MeView, RegisterView, DispatchView
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register"),         
+    path("register/", RegisterView.as_view(), name="register"),
+    path("me/", MeView.as_view(), name="me"), 
+    path("dispatch/", DispatchView.as_view(), name="dispatch"),        
 ]
