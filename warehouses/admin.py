4bb8112decbc0d9b4116a6442c381287c7e9a93c
mucheru-delvan <mkdelvan9@gmail.com>
2026-09-19 20:21:36 +0300

    Registered warehouses in admin.py

--- warehouses/admin.py
+++ warehouses/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Warehouse
+
+
+@admin.register(Warehouse)
+class WarehouseAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "code",
+        "location",
+        "latitude",
+        "longitude",
+        "is_active",
+        "created_at",
+    )
+
+    search_fields = (
+        "name",
+        "code",
+        "location",
+    )
+
+    list_filter = (
+        "is_active",
+    )
--- warehouses/models.py
+++ warehouses/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Warehouse(models.Model):
+    name = models.CharField(max_length=100)
+
+    code = models.CharField(
+        max_length=20,
+        unique=True,
+    )
+
+    location = models.CharField(max_length=100)
+
+    address = models.TextField(blank=True)
+
+    latitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+    )
+
+    longitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+    )
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.code})"
