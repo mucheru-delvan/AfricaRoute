bc221bf962622659a29152bab9115240815360ec
mucheru-delvan <mkdelvan9@gmail.com>
2026-09-25 18:19:50 +0300

    Add OpenAPI documentation and Swagger JWT auth

--- users/urls.py
+++ users/urls.py
@@ -1,17 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
-from .views import MeView, RegisterView, DispatchView
+from .views import RegisterView, MeView, DispatchView
 
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("me/", MeView.as_view(), name="me"),
     path("dispatch/", DispatchView.as_view(), name="dispatch"),
-
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
