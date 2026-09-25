7f7f956b1fe975e7fb0754ef426782fdbb57d408
mucheru-delvan <mkdelvan9@gmail.com>
2026-09-25 07:34:56 +0300

    Registered the model to admin.py

--- deliveries/admin.py
+++ deliveries/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Delivery
+
+
+@admin.register(Delivery)
+class DeliveryAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "route_stop",
+        "status",
+        "recipient_name",
+        "delivered_at",
+        "failed_at",
+    )
+
+    list_filter = (
+        "status",
+        "failure_reason",
+    )
+
+    search_fields = (
+        "recipient_name",
+        "route_stop__order__customer__name",
+    )
